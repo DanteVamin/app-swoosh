d5ede4224d9d502273ef466d20585026f23bf2bc
Adam Vinten <adamvinten@adams-mbp.home>
2018-01-11 10:52:05 +0000

    lesson 06

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Adam Vinten on 11/01/2018.
+//  Copyright © 2018 Adam Vinten. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectSkillLevel: String!
+    
+}
