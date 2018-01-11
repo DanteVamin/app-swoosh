6deb61168b58a1a21be5230a414afda716b0d613
Adam Vinten <adamvinten@adams-mbp.home>
2018-01-11 10:12:19 +0000

    lesson 5

---
+++ app-swoosh/Controller/WelcomeVC.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeVC.swift
+//  app-swoosh
+//
+//  Created by Adam Vinten on 09/01/2018.
+//  Copyright © 2018 Adam Vinten. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    @IBAction func unwindeFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+}
+
